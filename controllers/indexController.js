612745531925259138020fa92cb795c43042dc4b
Ahnaf <mohammadahnaf123@gmail.com>
2024-09-20 15:30:42 +0400

    Create login and register forms
    Created login and register forms
    Created routes for get and post method for both login and register
    Used custom validation in express validator to validate register form fields

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,32 @@
 const { getAllMessages } = require("../db/queries")
+const {body, validationResult} = require("express-validator");
+
+const validation = [
+    body("firstname").trim()
+    .notEmpty().withMessage("first name field cannot be empty")
+    .bail()
+    .isAlpha().withMessage("first name field only accpets letters A-Z"),
+
+    body("lastname").trim()
+    .notEmpty().withMessage("last name field cannot be empty")
+    .bail()
+    .isAlpha().withMessage("last name field only accpets letters A-Z"),
+
+    body("email").trim()
+    .notEmpty().withMessage("email field cannot be empty")
+    .bail()
+    .isEmail().withMessage("please input a valid email"),
+
+    body("pass").trim(),
+
+    body("confirm-pass").trim()
+    .custom(async (value, { req }) => {
+        if (value !== req.body.pass) {
+            throw new Error("passwords do not match");
+        }
+        return true;
+    })
+]
 
 
 const getMessages = async (req, res) => {
@@ -7,6 +35,32 @@ const getMessages = async (req, res) => {
 
 }
 
+const loginForm = async (req, res) => {
+    res.render("log-in");
+}
+
+const registerForm = async (req, res) => {
+    res.render("register");
+}
+
+const submitRegisterForm = [
+    validation,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render("register", {error: errors.array()});
+        }   else {
+            res.redirect("/log-in");
+        }
+    }
+]
+
+
+
+
 module.exports = {
-    getMessages
+    getMessages,
+    loginForm,
+    registerForm,
+    submitRegisterForm
 }
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getMessages } = require("../controllers/indexController");
+const { getMessages, loginForm, registerForm, submitRegisterForm } = require("../controllers/indexController");
 const router = express.Router();
 
 router.get("/", getMessages);
+
+router.get("/log-in", loginForm);
+
+router.route("/register").get(registerForm).post(submitRegisterForm);
 
 
 module.exports = router;
